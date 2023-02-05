11787257d845569c18cb41bf7d93850074b8051d
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-02-05 14:33:37 +0100

    Add environment.d.ts

---
+++ environment.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    CONTENTFUL_SPACE_ID: string;
+    CONTENTFUL_CONTENT_DELIVERY_TOKEN: string;
+    NEXT_PUBLIC_EMAILJS_SERVICE_ID: string;
+    NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: string;
+    NEXT_PUBLIC_EMAILJS_PUBLIC_KEY: string;
+  }
+}
