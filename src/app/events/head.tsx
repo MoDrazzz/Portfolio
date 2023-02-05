0d92c78a406e38e2ead8aadc0ac9fa5c7cc05f46
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-02-05 15:27:11 +0100

    Change titles

--- src/app/events/head.tsx
+++ src/app/events/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Projects - Grzegorz Pawlik</title>
+      <title>Events - Grzegorz Pawlik</title>
       <meta name="description" content="Look over events I participate to!" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Grzegorz Pawlik - Front-End Developerc</title>
+      <title>Grzegorz Pawlik - Front-End Developer</title>
       <meta
         name="description"
         content="My name is Grzegorz Pawlik. I am a 17-year-old front-end developer based in Bielsko-Biała, Poland. Meet me closer!"
